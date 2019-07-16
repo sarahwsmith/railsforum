823206ddeff9d0251c93562e4ac4eb6a2183bc75
Sarah S <46499583+sarahwsmith@users.noreply.github.com>
2019-07-16 13:36:41 -0500

    updated css of index page and created topic model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_175810) do
+ActiveRecord::Schema.define(version: 2019_07_16_151036) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "context"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.datetime "last_post_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
